94eea414e1aa183a928d898022b98c8448dc698b
Neamen <71108342+ghostomar412@users.noreply.github.com>
2025-07-26 14:06:33 -0400

    Update AI.java

--- src/main/java/com/qube/simulator/AI.java
+++ src/main/java/com/qube/simulator/AI.java
@@ -4,7 +4,21 @@ public class AI{
 
 public static void main(String[] args){
 
-    
+    int count1 = 0;
+
+for (int i = 0; i < 1000; i++) {
+    Qubit q = new Qubit(); // Initializes to |0⟩
+
+    q.HGate();
+    q.ZGate();
+    q.HGate();
+
+    String result = q.executeSingle();
+
+    if (result.equals("1")) count1++;
+}
+
+System.out.println("Measured 1: " + count1);
 
 
 }       
